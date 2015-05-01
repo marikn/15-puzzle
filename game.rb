98809818b6775bc679e789380f68f6cdfed9202e
marik <marik.nazarm@gmail.com>
2015-05-01 22:47:34 +0300

    Add gitignore and swap! method witch can swap two elements in matrix

--- game.rb
+++ game.rb
@@ -7,14 +7,14 @@ class Puzzle
   attr_accessor :moves
 
   @puzzle
-  @empty_field
+  @ef
 
   def initialize
     self.window = Window.new(100,100,2,5)
     self.moves  = 0
 
-    @puzzle      = (1..15).to_a
-    @empty_field = [3,3]
+    @puzzle = (1..15).to_a
+    @ef 	  = [3,3]
 
     shuffle!
   end
@@ -23,7 +23,7 @@ class Puzzle
     self.window.clear
     @y = 0
     @puzzle.each { |xval|
-      @y +=5
+      @y+=5
       xval.each_with_index { |yval, yindex|
         self.window.setpos(yindex + 2, @y)
         self.window.addstr(yval.to_s)
@@ -43,38 +43,34 @@ class Puzzle
   def move!(direction)
     case direction
       when 'w'
-        if @empty_field[1] != 3
-          tmp = @puzzle[@empty_field[0]][@empty_field[1]+1]
-          @puzzle[@empty_field[0]][@empty_field[1]] = tmp
-          @puzzle[@empty_field[0]][@empty_field[1]+1] = nil
-          @empty_field[1] = @empty_field[1]+1
-          self.moves+=1
+        if @ef[1] != 3
+	        swap!(@ef[0], @ef[1]+1, @ef[1], 'v')
         end
       when 's'
-        if @empty_field[1] != 0
-          tmp = @puzzle[@empty_field[0]][@empty_field[1]-1]
-          @puzzle[@empty_field[0]][@empty_field[1]] = tmp
-          @puzzle[@empty_field[0]][@empty_field[1]-1] = nil
-          @empty_field[1] = @empty_field[1]-1
-          self.moves+=1
+        if @ef[1] != 0
+	        swap!(@ef[0], @ef[1]-1, @ef[1], 'v')
         end
       when 'a'
-        if @empty_field[0] != 3
-          tmp = @puzzle[@empty_field[0]+1][@empty_field[1]]
-          @puzzle[@empty_field[0]][@empty_field[1]] = tmp
-          @puzzle[@empty_field[0]+1][@empty_field[1]] = nil
-          @empty_field[0] = @empty_field[0]+1
-          self.moves+=1
+        if @ef[0] != 3
+	        swap!(@ef[0]+1, @ef[1], @ef[0], 'h')
         end
       when 'd'
-        if @empty_field[0] != 0
-          tmp = @puzzle[@empty_field[0]-1][@empty_field[1]]
-          @puzzle[@empty_field[0]][@empty_field[1]] = tmp
-          @puzzle[@empty_field[0]-1][@empty_field[1]] = nil
-          @empty_field[0] = @empty_field[0]-1
-          self.moves+=1
+        if @ef[0] != 0
+	        swap!(@ef[0]-1, @ef[1], @ef[0], 'h')
         end
     end
+  end
+
+  def swap!(x, y, z, direction)
+    case direction
+      when 'v'
+        @puzzle[x][y], @puzzle[x][z] = @puzzle[x][z], @puzzle[x][y]
+      when 'h'
+        @puzzle[x][y], @puzzle[z][y] = @puzzle[z][y], @puzzle[x][y]
+    end 
+
+    @ef = [x, y]
+    self.moves+=1
   end
 end
 
